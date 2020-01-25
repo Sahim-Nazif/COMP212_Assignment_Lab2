ac2371a1c611934373d308f7c3595e1ba28bfa24
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-01-25 15:01:05 -0500

    created local variables and referenced (xaml) elments

--- COMP212_Assignment_Lab2/MainWindow.xaml.cs
+++ COMP212_Assignment_Lab2/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace COMP212_Assignment_Lab2
     /// </summary>
     public partial class MainWindow : Window
     {
+        //creating local variables
+        double amount, balance;
         public MainWindow()
         {
             InitializeComponent();
@@ -27,11 +29,25 @@ namespace COMP212_Assignment_Lab2
 
         private void BtnConfirm_Transaction(object sender, RoutedEventArgs e)
         {
+            //setting variables
+            amount = double.Parse(txtAmount.Text);
+            balance = double.Parse(txtBalance.Text);
+            
+            /*Creating dialog messages to be passed in MessageBox*/
+            string withdrawFromSaving = "Do you want to withdraw $ " + amount + "from" + rdSaving.Content;
+            string withdrawFromChequing= "Do you want to withdraw $ " + amount + "from" + rdChequing.Content;
+            string depositToSaving= "Do you want to desposit $ " + amount + "to" + rdSaving.Content;
+            string depositToChequing = "Do you want to desposit $ " + amount + "to" + rdChequing.Content;
+
+
 
         }
 
+        /*Will close the application*/
         private void Exit_Click(object sender, RoutedEventArgs e)
         {
+            MessageBox.Show("Thanks for using the application. Have a good day", "Exit", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+            this.Hide();
 
         }
     }
